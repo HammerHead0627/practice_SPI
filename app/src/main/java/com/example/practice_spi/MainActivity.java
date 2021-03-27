50589b00830d844f81136e39dab6d96086444e9d
HammerHead <legox06270627@gmail.com>
2021-03-28 05:44:58 +0900

    add MainActivity

--- app/src/main/java/com/example/practice_spi/MainActivity.java
+++ app/src/main/java/com/example/practice_spi/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.practice_spi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView countLabel, questionLabel;
+    private Button answerButton1;
+    private Button answerButton2;
+    private Button answerButton3;
+    private Button answerButton4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void checkAnswer(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

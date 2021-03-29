2c555bafae8508d98dd2bab72eb203889448013c
HammerHead <legox06270627@gmail.com>
2021-03-30 02:42:46 +0900

    edit ResultActivity
    add returnTop()

--- app/src/main/java/com/example/practice_spi/ResultActivity.java
+++ app/src/main/java/com/example/practice_spi/ResultActivity.java
@@ -2,7 +2,11 @@ package com.example.practice_spi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -13,9 +17,28 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         TextView resultLabel = findViewById(R.id.resultLabel);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel);
+
         //正解数を取得
         int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
-        resultLabel.setText(score + " / " + MainActivity.getQuizCount());
 
+        //トータルスコアの読み出し
+        SharedPreferences prefs = getSharedPreferences("quizApp", Context.MODE_PRIVATE);
+        int totalScore = prefs.getInt("totalScore", 0);
+
+        //トータルスコアに今回のスコアを加算
+        totalScore += score ;
+
+        //textviewに表示する
+        resultLabel.setText(score + " / " + MainActivity.getQuizCount());
+        totalScoreLabel.setText("トータルスコア:" + totalScore);
+
+        //トータルスコアを保存する
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("totalScore", totalScore);
+        editor.apply();
+    }
+    public void returnTop(View view) {
+        startActivity(new Intent(ResultActivity.this, MainActivity.class));
     }
 }
